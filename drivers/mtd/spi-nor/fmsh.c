7ed9fdb35f4aac38bc5080ec5dd21d3aec3dce9d
Brian 'Redbeard' Harrington <redbeard@dead-city.org>
2024-08-19 23:42:20 -0700

    fix broken macro
    spacemit is adding a new spi-nor flash type.  that's all well and good
    but i can't find where the macro was being run from.

--- drivers/mtd/spi-nor/fmsh.c
+++ drivers/mtd/spi-nor/fmsh.c
@@ -8,11 +8,15 @@
 #include "core.h"
 
 static const struct flash_info fmsh_nor_parts[] = {
-	{ "FM25Q64AI3", INFO(0xa14017, 0, 4 * 1024,  2048)
-		NO_SFDP_FLAGS(SECT_4K | SPI_NOR_QUAD_READ |
-			      SPI_NOR_DUAL_READ) },
+	{
+		.id = SNOR_ID(0xA1, 0x40, 0x17),
+		.name = "FM25Q64AI3",
+		.size = SZ_4M,
+		.no_sfdp_flags = SECT_4K | SPI_NOR_DUAL_READ | SPI_NOR_QUAD_READ,
+	},
 };
 
+/* FMSH (Shanghai Fudan Microelectronics Group Co., Ltd) */
 const struct spi_nor_manufacturer spi_nor_fmsh = {
 	.name = "fmsh",
 	.parts = fmsh_nor_parts,
